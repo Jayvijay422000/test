735528d035f83b7db397a88e4c07750f036fe4a2
JayPatel618 <Jayvijay422000@gmail.com>
2023-03-24 17:44:24 +0530

    Add files via upload
    new 1

---
+++ lec3.jsx
@@ -0,0 +1,72 @@
+x=110;
+y=200;
+
+x++;
+console.log(x);
+
+y--;
+console.log(y);
+
+
+//x++ matlab x ma ek vadharvo
+
+//x-- matlab x ma ek ocho karvo
+
+
+
+//Assignment operator
+
+//Assignment matlab sopavu 
+
+//i have assigned a work of floor cleaner to mehul .
+
+//me mehul ne floor uppar potu marvanu nu kam sopiyu che.
+
+//i have asssigned a value 43 to x
+
+// this means that x=43 .
+
+//1) assigned a value to x
+
+x=43;
+console.log(x);
+
+//2) x+=y => x=x+y
+
+x=56
+
+y= 34
+
+x+=y  // x=x+y 
+
+console.log(x)
+
+
+//3)x-=y => x=x-y
+
+
+//4)x*=y = > x=x*y
+
+//5) x/=y => x=x/y
+
+//6) x%=y => x=x%y
+
+//7) x**=y  => x=x**y
+
+//explain this number 2.7812838944369352e+66 =2.7812838944369352 * 10^66 =2781283894436935200000000000000000000000000000000000000000000000000
+
+
+*1) shree dhruvil kumar
+2) shreedevi monika ben
+3) shree mehul maharaj
+4) shree vaibhav maharaj*
+
+*phele tamre khali-jagya bharvu*
+*aa badhu uppar revision kari . tamri svadhyay-pothi ma lakhvu*
+
+*dhanyavad*
+
+
+
+
+//1e-10  = 1 * 10^(-10) =1/(10^10)=0.10000000000
